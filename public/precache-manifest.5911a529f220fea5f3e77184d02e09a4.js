616636ca4f76e26c63bdcbf1e6a52c46400a3452
Natasha <natashanodine@gmail.com>
2019-12-03 12:55:21 -0500

    first commit

---
+++ public/precache-manifest.5911a529f220fea5f3e77184d02e09a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f42637776b100c8355bb4393d54173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13889056f2c9f3ecb325",
+    "url": "/static/css/2.b883d912.chunk.css"
+  },
+  {
+    "revision": "6557296d13b1429993c2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "13889056f2c9f3ecb325",
+    "url": "/static/js/2.1cdad18f.chunk.js"
+  },
+  {
+    "revision": "6557296d13b1429993c2",
+    "url": "/static/js/main.babbe1ba.chunk.js"
+  },
+  {
+    "revision": "d91195501b54a3bf246e",
+    "url": "/static/js/runtime-main.a64f8ae1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
